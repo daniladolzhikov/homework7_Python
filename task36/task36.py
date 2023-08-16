22551dda0c1673ecc66d3de813ea36ec6c2ae333
daniladolzhikov <d.dolzhikov17@gmail.com>
2023-08-16 19:00:10 +0200

    added a function print_operation_table

--- task36/task36.py
+++ task36/task36.py
@@ -15,3 +15,12 @@
 # 4 8 12 16 20 24
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for row in range(1, num_rows + 1):
+        for col in range(1, num_columns + 1):
+            result = operation(row, col)
+            print(result, end=" ")
+        print() 
+
+print_operation_table(lambda x, y: x * y)
