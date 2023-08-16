162f9752f6a24eef55436160129a82b6a2785812
daniladolzhikov <d.dolzhikov17@gmail.com>
2023-08-16 18:28:48 +0200

    added rhythm check function

--- task34/task34.py
+++ task34/task34.py
@@ -5,3 +5,13 @@ def count_vowels(word):
         if letter.lower() in vowels:
             count += 1
     return count
+
+
+def check_rhythm(poem):
+    phrases = poem.split()
+    vowel_counts = [count_vowels(phrase.replace('-', '')) for phrase in phrases]
+
+    if all(count == vowel_counts[0] for count in vowel_counts):
+        return "Парам пам-пам"
+    else:
+        return "Пам парам"
